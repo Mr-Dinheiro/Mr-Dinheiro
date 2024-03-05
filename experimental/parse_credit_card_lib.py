c60d4c495926fa99bac17679e4627a232355baeb
valerieps <souza.pereira.valeria@gmail.com>
2024-03-05 20:54:01 -0300

    Adds accrual month representation stuff.

--- experimental/parse_credit_card_lib.py
+++ experimental/parse_credit_card_lib.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import zoneinfo
+import dataclasses
 
+@dataclasses.dataclass
+class AccrualMonth:
+    year: int
+    month: int
+
+    # def __repr__(self):
+    #     return f"{self.year}/{self.month}"
+    def __hash__(self):
+        return hash(self.year*100 + self.month)
+
+    def __eq__(self, other):
+        return self.year == other.year and self.month == other.month
 
 def get_data(file_path: str):
     df = pd.read_csv(file_path)
@@ -26,5 +39,15 @@ def get_transactions_per_category(df: pd.DataFrame, category: str) -> pd.DataFra
 def batch_update_category(df: pd.DataFrame, from_category: str, to_category:str) -> None:
     """Changes the category from all transactions with from_category."""
     add_new_category(df, to_category)
-    
     return df.mask(df == from_category, to_category)
+
+def date_to_accrual_month(date: pd.Timestamp):
+    return AccrualMonth(date.date().year, date.date().month)
+
+def add_accrual_month_column(df: pd.DataFrame):
+    ts = pd.Series(date_to_accrual_month(df['date']))
+    df.insert(2, 'accrual_month', ts)
+
+
+def get_total_by_accrual_month(df: pd.DataFrame) -> pd.DataFrame:
+    return df.groupby('accrual_month')['amount'].agg(sum)
